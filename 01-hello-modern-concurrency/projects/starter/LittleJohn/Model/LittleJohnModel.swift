57848d08859f0f714d97bc2686f3123435f2aaeb
joongseod <ehdwndtj@naver.com>
2022-01-09 08:20:15 +0900

    async sequences

--- 01-hello-modern-concurrency/projects/starter/LittleJohn/Model/LittleJohnModel.swift
+++ 01-hello-modern-concurrency/projects/starter/LittleJohn/Model/LittleJohnModel.swift
@@ -46,6 +46,24 @@ class LittleJohnModel: ObservableObject {
     guard let url = URL(string: "http://localhost:8080/littlejohn/ticker?\(selectedSymbols.joined(separator: ","))") else {
       throw "The URL could not be created."
     }
+    
+    let (stream, response) = try await liveURLSession.bytes(from: url)
+    guard (response as? HTTPURLResponse)?.statusCode == 200 else { throw "The server responded with an error" }
+    
+    for try await line in stream.lines {
+      let sortedSymbols = try JSONDecoder()
+        .decode([Stock].self, from: Data(line.utf8))
+        .sorted(by: { $0.name < $1.name })
+      
+      await MainActor.run {
+        tickerSymbols = sortedSymbols
+        print("Updated: \(Date())")
+      }
+    }
+    
+    await MainActor.run {
+      tickerSymbols = []
+    }
   }
 
   /// A URL session that lets requests run indefinitely so we can receive live updates from server.
